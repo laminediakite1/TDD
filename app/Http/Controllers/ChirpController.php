bfebbc86aab4303163aa6fcac42f44fd260485ec
laminediakite1 <laminediakiteg2001@gamil.com>
2024-12-07 13:01:34 +0100

    premier commit

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function show(chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(chirp $chirp)
+    {
+        //
+    }
+}
