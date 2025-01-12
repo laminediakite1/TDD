3d1a3259d90b5a59cfe644628d54f283fb0983e4
laminediakite1 <laminediakiteg2001@gamil.com>
2025-01-12 18:45:25 +0100

    Premier commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps')">
-                         {{__('chirps')}}
+                         {{__('Incognitos')}}
                      </x-nav-link>
                 </div>
             </div>
